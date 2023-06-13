19881b022ba1e73a7dc10be2b1e1bde99680b239
reddymodi <114898950+reddymodi@users.noreply.github.com>
2023-06-13 13:16:51 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,7 +1,14 @@
 variable "region" {
+  type = string
   default = "ap-northeast-1"
 }
 
 variable "ami_id" {
+  type = string
   default = "ami-0d739893974bd27d0"
+}
+
+variable "availability_zone" {
+    type = string
+    default = "ap-northeast-1a"
 }
